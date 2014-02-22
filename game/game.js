b5374911705091334819b0b01c0e4e1c17a5ea90
jsguy <mikkel@pointful.com>
2014-02-22 12:55:10 +1100

    We now have draggable words.

--- game/game.js
+++ game/game.js
@@ -1,198 +1,101 @@
+//  
+//  WordUp 0.0.0.0.0.0.1
+//
 
-//  This creates the actual game
 var game = new Phaser.Game(
-    800, 600, Phaser.AUTO, 
+    800, 600, 
+    Phaser.AUTO, 
     'phaser-example', 
     { 
-        //  Stuff we load into the game
-        preload: function preload() {
-            game.load.atlas(
-                'breakout', 
-                'resources/breakout.png', 
-                'resources/breakout.json'
-            );
-            game.load.image(
-                'starfield', 
-                'resources/starfield.jpg'
-            );
-        },
-        //  Setup the game world
-        create: create, 
-        //  Run for every frame
-        update: update 
+        preload: preload, 
+        create: create 
+    });
+
+var tileX = 64, tileY = 64;
+
+function preload() {
+
+    game.load.image('grid', 'phaser/examples/assets/tests/debug-grid-1920x1920.png');
+    game.load.image('atari1', 'phaser/examples/assets/sprites/atari130xe.png');
+    game.load.image('atari2', 'phaser/examples/assets/sprites/atari800xl.png');
+
+    game.load.atlas(
+        'letters', 
+        'resources/letter_tiles.png', 
+        'resources/letter_tiles.json'
+    );
+
+}
+
+//  Custom function to create a word
+var createWord = function(word, offsetX, offsetY) {
+    if(!word) {
+        return;
     }
-);
 
-//  The pattern here seems to be global variables 
-//  I'd like to use knockout view model(s) here
+    offsetX = offsetX || 0;
+    offsetY = offsetY || 0;
 
-var ball, paddle, bricks, ballOnPaddle = true, 
-    lives = 3, score = 0, 
-    scoreText, livesText, introText, starField;
+    //  Create and add a word group
+    var myWord = game.add.group();
+
+    word = word.toLowerCase();
+
+    //  Create all letters
+    for(var i = 0; i < word.length; i += 1) {
+        myWord.create(
+            offsetX + i * tileX, 
+            offsetY, 
+            'letters', 
+            word.charAt(i) + '.png'
+        );
+    }
+
+    //  You cannot drag a group: 
+    //  http://www.html5gamedevs.com/topic/2336-input-andor-drag-on-a-group/
+    //  So we create a sprite that overlays the word, and 
+    //  track it uising sprite.events.onInputDown
+    var myWordSprite = game.add.sprite(offsetX, offsetY);
+    myWordSprite.width = tileX * word.length;
+    myWordSprite.height = tileY;
+
+    //  Enable dragging
+    myWordSprite.inputEnabled = true;
+    myWordSprite.input.useHandCursor = true;
+    myWordSprite.input.enableDrag();
+
+    //  Snap only when released, not on drag.
+    myWordSprite.input.enableSnap(tileX, tileY, false, true);
+
+    myWordSprite.hasDragged = false;
+
+    //  When you start dragging the sprite
+    myWordSprite.events.onInputDown.add(function(){
+        clearInterval(myWordSprite.updateTime);
+        if(!myWordSprite.hasDragged) {
+            myWordSprite.origX = myWordSprite.x;
+            myWordSprite.origY = myWordSprite.y;
+            myWordSprite.hasDragged = true;
+        }
+        myWordSprite.updateTime = setInterval(function(){
+            myWord.x = myWordSprite.x - myWordSprite.origX;
+            myWord.y = myWordSprite.y - myWordSprite.origY;
+        }, 13);
+    });
+
+    myWordSprite.events.onInputUp.add(function(){
+        clearInterval(myWordSprite.updateTime);
+        myWord.x = myWordSprite.x - myWordSprite.origX;
+        myWord.y = myWordSprite.y - myWordSprite.origY;
+    });
+};
+
 
 function create() {
+    //  Setup grid
+    game.add.sprite(0, 0, 'grid');
 
-    //  This tells the world to include walls on the left, right and top, but not the bottom - so the ball can 'fall' away.
-    game.physics.setBoundsToWorld(true, true, true, false);
-
-    starField = game.add.tileSprite(0, 0, 800, 600, 'starfield');
-
-    var brick;
-    bricks = game.add.group();
-
-    for (var y = 0; y < 4; y++)
-    {
-        for (var x = 0; x < 15; x++)
-        {
-            brick = bricks.create(120 + (x * 36), 100 + (y * 52), 'breakout', 'brick_' + (y+1) + '_1.png');
-            brick.body.bounce.setTo(1, 1);
-            brick.body.immovable = true;
-        }
-    }
-
-    paddle = game.add.sprite(game.world.centerX, 500, 'breakout', 'paddle_big.png');
-    paddle.anchor.setTo(0.5, 0.5);
-    paddle.body.collideWorldBounds = true;
-    paddle.body.bounce.setTo(1, 1);
-    paddle.body.immovable = true;
-
-    ball = game.add.sprite(game.world.centerX, paddle.y - 16, 'breakout', 'ball_1.png');
-    ball.anchor.setTo(0.5, 0.5);
-    ball.body.collideWorldBounds = true;
-    ball.body.bounce.setTo(1, 1);
-    ball.animations.add('spin', [ 'ball_1.png', 'ball_2.png', 'ball_3.png', 'ball_4.png', 'ball_5.png' ], 50, true, false);
-    ball.events.onOutOfBounds.add(ballLost, this);
-
-    scoreText = game.add.text(32, 550, 'score: 0', { font: "20px Arial", fill: "#ffffff", align: "left" });
-    livesText = game.add.text(680, 550, 'lives: 3', { font: "20px Arial", fill: "#ffffff", align: "left" });
-    introText = game.add.text(game.world.centerX, 400, '- click to start -', { font: "40px Arial", fill: "#ffffff", align: "center" });
-    introText.anchor.setTo(0.5, 0.5);
-
-    game.input.onDown.add(releaseBall, this);
-
-}
-
-//  Note: this is run for every frame...
-function update () {
-
-    //  Fun, but a little sea-sick inducing :) Uncomment if you like!
-    //  starField.tilePosition.x += (game.input.speed.x / 2);
-
-    paddle.x = game.input.x;
-
-    if (paddle.x < 24)
-    {
-        paddle.x = 24;
-    }
-    else if (paddle.x > game.width - 24)
-    {
-        paddle.x = game.width - 24;
-    }
-
-    if (ballOnPaddle)
-    {
-        ball.x = paddle.x;
-    }
-    else
-    {
-        game.physics.collide(ball, paddle, ballHitPaddle, null, this);
-        game.physics.collide(ball, bricks, ballHitBrick, null, this);
-    }
-
-}
-
-function releaseBall () {
-
-    if (ballOnPaddle)
-    {
-        ballOnPaddle = false;
-        ball.body.velocity.y = -300;
-        ball.body.velocity.x = -75;
-        ball.animations.play('spin');
-        introText.visible = false;
-    }
-
-}
-
-function ballLost () {
-
-    lives--;
-
-    if (lives === 0)
-    {
-        gameOver();
-    }
-    else
-    {
-        livesText.content = 'lives: ' + lives;
-        ballOnPaddle = true;
-        ball.x = paddle.x + 16;
-        ball.y = paddle.y - 16;
-        ball.body.reset();
-        ball.animations.stop();
-    }
-
-}
-
-function gameOver () {
-
-    ball.body.velocity.setTo(0, 0);
-    
-    introText.content = 'Game Over!';
-    introText.visible = true;
-
-}
-
-function ballHitBrick (_ball, _brick) {
-
-    _brick.kill();
-
-    score += 10;
-
-    scoreText.content = 'score: ' + score;
-
-    //  Are they any bricks left?
-    if (bricks.countLiving() == 0)
-    {
-        //  New level starts
-        score += 1000;
-        scoreText.content = 'score: ' + score;
-        introText.content = '- Next Level -';
-
-        //  Let's move the ball back to the paddle
-        ballOnPaddle = true;
-        ball.body.velocity.setTo(0, 0);
-        ball.x = paddle.x + 16;
-        ball.y = paddle.y - 16;
-        ball.animations.stop();
-
-        //  And bring the bricks back from the dead :)
-        bricks.callAll('revive');
-    }
-
-}
-
-function ballHitPaddle (_ball, _paddle) {
-
-    var diff = 0;
-
-    if (_ball.x < _paddle.x)
-    {
-        //  Ball is on the left-hand side of the paddle
-        diff = _paddle.x - _ball.x;
-        _ball.body.velocity.x = (-10 * diff);
-    }
-    else if (_ball.x > _paddle.x)
-    {
-        //  Ball is on the right-hand side of the paddle
-        diff = _ball.x -_paddle.x;
-        _ball.body.velocity.x = (10 * diff);
-    }
-    else
-    {
-        //  Ball is perfectly in the middle
-        //  Add a little random X to stop it bouncing straight up!
-        _ball.body.velocity.x = 2 + Math.random() * 8;
-    }
-
-}
+    //  Add some words
+    createWord("bitches", 64, 64);
+    createWord("wordup", 64, 64);
+};
